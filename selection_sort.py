76fa6edd62ed11d4f9dd2b09f90477a3c3bd6a1e
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-01-16 18:37:53 +0000

    completed selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,2 +1,11 @@
 def sort(L):
+    n = len(L)
+    for i in range(n - 1):
+        x = L[i]
+        p = i
+        for j in range(i + 1, n):
+            if L[j] < x:
+                x = L[j]
+                p = j
+        L[i], L[p] = L[p], L[i]
     return L
